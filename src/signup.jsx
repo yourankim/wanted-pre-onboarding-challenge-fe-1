7bb4702b0efa7017a412a6155ec51d7d8b58cfda
yourankim <youran.dev@gmail.com>
2022-08-08 23:56:33 +0900

    update: signup.jsx

--- src/signup.jsx
+++ src/signup.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState();
+  const handleChanged = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const signup = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/users/create',
+        formData
+      );
+      alert(response.data.message);
+      navigate('/login');
+    } catch (error) {
+      alert(error);
+      const { data } = error.response;
+      alert(data.details);
+    }
+  };
+
   return (
     <section>
-      <form>
-        <input type='email'></input>
-        <input type='password'></input>
+      <form onSubmit={signup}>
+        <input
+          id='email'
+          type='email'
+          placeholder='email'
+          onChange={handleChanged}></input>
+        <input
+          id='password'
+          type='password'
+          placeholder='password'
+          onChange={handleChanged}></input>
         <button>회원가입</button>
       </form>
     </section>
